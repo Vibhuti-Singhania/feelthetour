6ec061d386e08fecb3d9395faf73ba5208902b11
Vibhuti-Singhania <vibhuti.singhania@gmail.com>
2018-06-20 16:27:28 +0530

    added_navigation_drawer

--- app/src/main/java/com/example/android/feelthetour/WelcomeActivity.java
+++ app/src/main/java/com/example/android/feelthetour/WelcomeActivity.java
@@ -4,16 +4,25 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class WelcomeActivity extends AppCompatActivity {
 //    private Button mlogoutfb;
 //    private FirebaseAuth mAuth;
+private SlidingUpPanelLayout panelLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,13 +35,26 @@ public class WelcomeActivity extends AppCompatActivity {
             startActivity(new Intent(this, LoginActivity.class));
         }
 
-        findViewById(R.id.buttonLogout).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-                SharedPrefManager.getInstance(getApplicationContext()).logout();
-            }
-        });
+//        findViewById(R.id.buttonLogout).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                finish();
+//                SharedPrefManager.getInstance(getApplicationContext()).logout();
+//            }
+//        });
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//         setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawer.addDrawerListener(toggle);
+//        toggle.syncState();
+        panelLayout = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+//        navigationView.setNavigationItemSelectedListener(this);
+
 //        mlogoutfb=(Button)findViewById(R.id.button_logout_fb);
 //        mAuth = FirebaseAuth.getInstance();
 //        mlogoutfb.setOnClickListener(new View.OnClickListener() {
@@ -43,7 +65,42 @@ public class WelcomeActivity extends AppCompatActivity {
 //    });
 
     }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_shuffle) {
+            return true;
+        }
+        if (id == R.id.action_equalizer) {
+            return true;
+        }
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 //    public void onStart() {
 //        super.onStart();
